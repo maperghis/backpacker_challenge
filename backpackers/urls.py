6d64591c03746b57811c2e82105bb11a6a252d17
Miranda Aperghis <miranda.aperghis@gmail.com>
2017-08-31 16:35:37 +0800

    Rewrote API using class-based views

--- backpackers/urls.py
+++ backpackers/urls.py
@@ -2,12 +2,11 @@ from django.conf.urls import patterns, url
 from backpackers import views
 
 urlpatterns = patterns('',
-    url(r'^person/$', views.personList, name='personList'),
-    url(r'^person/(?P<name>[a-zA-Z]+\s[a-zA-Z]+)/$', views.personDetail,
-        name='personDetail'),
-    url(r'^state/$', views.stateList, name='stateList'),
-    url(r'^state/(?P<name>[a-zA-Z]+)/$', views.stateDetail, name='stateDetail'),
-    url(r'^transport/$', views.transportList, name='transportList'),
-    url(r'^transport/(?P<mode>[a-zA-Z]+)/$', views.transportDetail,
-        name='transportDetail'),
+    url(r'^person/$', views.PersonList.as_view()),
+    url(r'^person/(?P<name>[a-zA-Z]+\s[a-zA-Z]+)/$',
+        views.PersonDetail.as_view()),
+    url(r'^state/$', views.StateList.as_view()),
+    url(r'^state/(?P<name>[a-zA-Z]+)/$', views.StateDetail.as_view()),
+    url(r'^transport/$', views.TransportList.as_view()),
+    url(r'^transport/(?P<mode>[a-zA-Z]+)/$', views.TransportDetail.as_view()),
     )
--- backpackers/views.py
+++ backpackers/views.py
@@ -1,69 +1,80 @@
-from django.http import HttpResponse
 from backpackers.models import Person, State, Transport
 from backpackers.serializers import PersonSerializer, StateSerializer, \
     TransportSerializer
-from rest_framework.decorators import api_view
+from django.http import Http404
+from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 
 
-@api_view(['GET'])
-def personList(request):
-    '''List all people'''
-    if request.method == 'GET':
+class PersonList(APIView):
+    '''List all the people'''
+
+    def get(self, request):
         people = Person.objects.all()
         serializer = PersonSerializer(people, many=True)
         return Response(serializer.data)
 
-@api_view(['GET'])
-def personDetail(request, name):
-    '''Retrieve a person'''
-    try:
-        firstName, lastName = name.split(' ')
-        person = Person.objects.get(firstName=firstName, lastName=lastName)
-    except Person.DoesNotExist as exc:
-        return Response(status=status.HTTP_404_NOT_FOUND)
 
-    if request.method == 'GET':
+class PersonDetail(APIView):
+    '''Retrieve a person instance'''
+
+    def getObject(self, name):
+        try:
+            firstName, lastName = name.split(' ')
+            return Person.objects.get(firstName=firstName, lastName=lastName)
+        except Person.DoesNotExist as exc:
+            raise Http404
+
+    def get(self, request, name):
+        person = self.getObject(name)
         serializer = PersonSerializer(person)
         return Response(serializer.data)
 
-@api_view(['GET'])
-def stateList(request):
-    '''List all states'''
-    if request.method == 'GET':
+
+class StateList(APIView):
+    '''List all the states'''
+
+    def get(self, request):
         states = State.objects.all()
         serializer = StateSerializer(states, many=True)
         return Response(serializer.data)
 
-@api_view(['GET'])
-def stateDetail(request, name):
-    '''Retreive a state'''
-    try:
-        state = State.objects.get(name=name)
-    except Stae.DoesNotExist as exc:
-        return Response(status=status.HTTP_404_NOT_FOUND)
 
-    if request.method == 'GET':
+class StateDetail(APIView):
+    '''Retrieve a state instance'''
+
+    def getObject(self, name):
+        try:
+            return State.objects.get(name=name)
+        except State.DoesNotExist as exc:
+            raise Http404
+
+    def get(self, request, name):
+        state = self.getObject(name)
         serializer = StateSerializer(state)
         return Response(serializer.data)
 
-@api_view(['GET'])
-def transportList(request):
-    '''List all transport modes'''
-    if request.method == 'GET':
+
+class TransportList(APIView):
+    '''Retrieve all the transport modes'''
+
+    def get(self, request):
         modes = Transport.objects.all()
         serializer = TransportSerializer(modes, many=True)
         return Response(serializer.data)
 
-@api_view(['GET'])
-def transportDetail(request, mode):
-    '''Retreive a transport mode'''
-    try:
-        state = Transport.objects.get(mode=mode)
-    except Transport.DoesNotExist as exc:
-        return Response(status=status.HTTP_404_NOT_FOUND)
 
-    if request.method == 'GET':
-        serializer = TransportSerializer(state)
+class TransportDetail(APIView):
+    '''Retrieve a transport instance'''
+
+    def getObject(self, mode):
+        try:
+            return Transport.objects.get(mode=mode)
+        except Transport.DoesNotExist as exc:
+            raise Http404
+
+    def get(self, request, mode):
+        name = self.getObject(mode)
+        serializer = TransportSerializer(name)
         return Response(serializer.data)
