cc5e300748757a1e52782c29fa92ddd0138aadc3
Miranda Aperghis <miranda.aperghis@gmail.com>
2017-08-31 16:16:27 +0800

    Included rest_framework in installed apps, using api views

--- backpackers/urls.py
+++ backpackers/urls.py
@@ -2,7 +2,6 @@ from django.conf.urls import patterns, url
 from backpackers import views
 
 urlpatterns = patterns('',
-    url(r'^$', views.index, name='index'),
     url(r'^person/$', views.personList, name='personList'),
     url(r'^person/(?P<name>[a-zA-Z]+\s[a-zA-Z]+)/$', views.personDetail,
         name='personDetail'),
--- backpackers/views.py
+++ backpackers/views.py
@@ -1,72 +1,69 @@
-from django.http import HttpResponse, JsonResponse
-from django.views.decorators.csrf import csrf_exempt
-from rest_framework.renderers import JSONRenderer
-from rest_framework.parsers import JSONParser
+from django.http import HttpResponse
 from backpackers.models import Person, State, Transport
 from backpackers.serializers import PersonSerializer, StateSerializer, \
     TransportSerializer
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
 
 
-def index(request):
-    return HttpResponse("Hello, world!")
-
-@csrf_exempt
+@api_view(['GET'])
 def personList(request):
     '''List all people'''
     if request.method == 'GET':
         people = Person.objects.all()
         serializer = PersonSerializer(people, many=True)
-        return JsonResponse(serializer.data, safe=False)
+        return Response(serializer.data)
 
-@csrf_exempt
+@api_view(['GET'])
 def personDetail(request, name):
     '''Retrieve a person'''
     try:
         firstName, lastName = name.split(' ')
         person = Person.objects.get(firstName=firstName, lastName=lastName)
-    except Exception as exc:
-        return HttpResponse(exc)
+    except Person.DoesNotExist as exc:
+        return Response(status=status.HTTP_404_NOT_FOUND)
 
     if request.method == 'GET':
         serializer = PersonSerializer(person)
-        return JsonResponse(serializer.data)
+        return Response(serializer.data)
 
-@csrf_exempt
+@api_view(['GET'])
 def stateList(request):
     '''List all states'''
     if request.method == 'GET':
         states = State.objects.all()
         serializer = StateSerializer(states, many=True)
-        return JsonResponse(serializer.data, safe=False)
+        return Response(serializer.data)
 
-@csrf_exempt
+@api_view(['GET'])
 def stateDetail(request, name):
     '''Retreive a state'''
     try:
         state = State.objects.get(name=name)
-    except Exception as exc:
-        return HttpResponse(exc)
+    except Stae.DoesNotExist as exc:
+        return Response(status=status.HTTP_404_NOT_FOUND)
 
     if request.method == 'GET':
         serializer = StateSerializer(state)
-        return JsonResponse(serializer.data)
+        return Response(serializer.data)
 
-@csrf_exempt
+@api_view(['GET'])
 def transportList(request):
     '''List all transport modes'''
     if request.method == 'GET':
         modes = Transport.objects.all()
         serializer = TransportSerializer(modes, many=True)
-        return JsonResponse(serializer.data, safe=False)
+        return Response(serializer.data)
 
-@csrf_exempt
+@api_view(['GET'])
 def transportDetail(request, mode):
     '''Retreive a transport mode'''
     try:
         state = Transport.objects.get(mode=mode)
-    except Exception as exc:
-        return HttpResponse(exc)
+    except Transport.DoesNotExist as exc:
+        return Response(status=status.HTTP_404_NOT_FOUND)
 
     if request.method == 'GET':
         serializer = TransportSerializer(state)
-        return JsonResponse(serializer.data)
+        return Response(serializer.data)
